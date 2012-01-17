bc33a54a271d1786b47abb29399d7814aff026b1
Tarcisio Gruppi <txgruppi@gmail.com>
2012-01-17 17:29:05 -0200

    Minor changes

--- sample/index.php
+++ sample/index.php
@@ -3,7 +3,7 @@
 define('BASE_PATH', dirname(__FILE__));
 define('LIB_PATH', BASE_PATH . '/lib');
 
-require_once LIB_PATH . '/fw.php';
+require_once BASE_PATH . '/../fw.php';
 require_once LIB_PATH . '/PublicArea.php';
 require_once LIB_PATH . '/MemberArea.php';
 
